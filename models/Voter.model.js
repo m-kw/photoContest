19877762332756ad18095a0fb0cee5b2c7a34d7c
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-03-16 16:28:08 +0100

    Fix bug - check voter's IP and allow voting on each picture only once

--- models/Voter.model.js
+++ models/Voter.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const voterSchema = new mongoose.Schema({
     user: { type: String, required: true },
-    votes: [{ type: Schema.Types.ObjeciId }],
+    votes: [{ type: mongoose.Schema.Types.ObjectId }],
 });
 
 module.exports = mongoose.model('Voter', voterSchema);
